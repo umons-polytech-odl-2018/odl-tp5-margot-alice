b0b524daa4c2e3787de2cf75113f3c0478f34837
172674 <Alice.TONDEUR@student.umons.ac.be>
2018-12-06 11:25:12 +0100

    TP5 exo1

--- src/main/java/exercise1/Student.java
+++ src/main/java/exercise1/Student.java
@@ -1,8 +1,10 @@
 package exercise1;
-
+import java.util.HashMap;
 import java.util.Optional;
 import java.util.OptionalInt;
 import java.util.Set;
+import java.util.stream.*;
+import java.util.*;
 
 /**
  * Represents a student.
@@ -14,10 +16,17 @@ public class Student {
     /**
      * Creates a new Student.
      *
+     *
      * @throws NullPointerException if one of the parameter is null.
      */
+    // name : String, registrationNumber : String (énoncé)
+    private String name;
+    private String registrationNumber;
+    //HashMap <Integer , Animal > zoo = new HashMap <Integer , Animal >();
+    HashMap <String,Integer> scorebycourse=new HashMap ();
     public Student(String name, String registrationNumber) {
-
+        this.name=name;
+        this.registrationNumber=registrationNumber;
     }
 
     /**
@@ -27,7 +36,11 @@ public class Student {
      * @throws NullPointerException if the course name is null.
      * @throws IllegalArgumentException if the score is less than 0 or greater than 20.
      */
-    public void setScore(String course, int score) {
+    //setScore(course : String, score : int) : void (énoncé)
+    public void setScore(String course, Integer score) {
+        //zoo.put(2,new Herbivore("Lapin", 2));
+
+        scorebycourse.put(course,score);
 
     }
 
@@ -35,19 +48,51 @@ public class Student {
      * Returns the score for the given course.
      *
      * @return the score if found, <code>OptionalInt#empty()</code> otherwise.
+     * @Override
+     * public int hashCode()
+      {
+     return Objects.hash(firstName,lastName);
+    }
      */
     public OptionalInt getScore(String course) {
-        return null;
+
+        Integer nullableScore=scorebycourse.get(course);
+
+        return nullableScore !=null? OptionalInt.of(nullableScore):OptionalInt.empty();
     }
 
     /**
      * Returns the average score.
      *
-     * @return the average score or 0 if there is none.
+     @return the average score or 0 if there is none.
      */
+
+
     public double averageScore() {
-        return 0;
-    }
+        int count =0;
+        double moyenne_course=0.0;
+        if(scorebycourse.size()==0){
+            return 0;
+        }
+
+
+            for (Integer score :scorebycourse.values()) {
+                count ++;
+
+                moyenne_course +=score ;
+
+
+            }
+
+            return moyenne_course/count;
+            /*
+            return scorebycourse.values().Stream
+            .MapToInt(Integer::intValue)
+            .average()
+            .orElse(0.0);
+             */
+        }
+
 
     /**
      * Returns the course with the highest score.
@@ -55,8 +100,13 @@ public class Student {
      * @return the best scored course or <code>Optional#empty()</code> if there is none.
      */
     public Optional<String> bestCourse() {
-        return null;
+
+        return scorebycourse.entrySet().stream()
+            .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+            .map(Map.Entry::getKey)
+            .findFirst();
     }
+
 
     /**
      * Returns the highest score.
@@ -64,7 +114,16 @@ public class Student {
      * @return the highest score or 0 if there is none.
      */
     public int bestScore() {
-        return 0;
+        int meilleur_score=0;
+        if(scorebycourse.size()==0){
+            return 0;
+        }
+        for (Integer score :scorebycourse.values()) {
+            if(meilleur_score<score){
+                meilleur_score=score;
+            }
+        }
+        return meilleur_score;
     }
 
     /**
@@ -72,27 +131,44 @@ public class Student {
      * A course is considered as passed if its score is higher than 12.
      */
     public Set<String> failedCourses() {
+        /*
+        List<Map.Entry<String, Integer>> filteredEntries = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : scorebycourse.entrySet()) {
+            if (entry.getValue() < 12) {
+                filteredEntries.add(entry);
+            }
+        }
         return null;
+        */
+        return scorebycourse.entrySet().stream()
+            .filter(entry->entry.getValue()<12)
+            .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+            .map(Map.Entry::getKey)
+            .collect(Collectors.toCollection(LinkedHashSet::new));
     }
-
     /**
      * Returns <code>true</code> if the student has an average score greater than or equal to 12.0 and has less than 3 failed courses.
      */
     public boolean isSuccessful() {
-        return false;
+       if(averageScore()>=12 && failedCourses().size()<3){
+           return true;
+       }
+       else{return false;}
     }
 
     /**
      * Returns the set of courses for which the student has received a score, sorted by course name.
      */
-    public Set<String> attendedCourses() { return null; }
-
-    public String getName() {
+    public Set<String> attendedCourses() {
         return null;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public String getRegistrationNumber() {
-        return null;
+        return registrationNumber;
     }
 
     @Override
